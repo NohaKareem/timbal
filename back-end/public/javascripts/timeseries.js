a0e729432b225c2cf4637f7befcfed677bbd25d2
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-04-08 17:16:07 -0400

    Experiment with timeseries styling

--- back-end/public/javascripts/timeseries.js
+++ back-end/public/javascripts/timeseries.js
@@ -91,7 +91,7 @@
     function render(classd, spaced, data, enableBrush) {
         
         var padding = timeRangePad(_.pluck(data, 'value'));
-
+        
         var margin = {
             top: 10,
             right: 25,
@@ -113,8 +113,9 @@
 
         var ticks = width > 800 ? 8 : 4;
 
-        x.domain(d3.extent([padding.minDate, padding.maxDate]));
+        x.domain(d3.extent([padding.minDate, padding.maxDate])); //~~ x domain
 
+        // [~adjust y axis given the nature of the data; spanning days/a single day]
         var xFormat, yFormat;
         if (lessThanDay(padding.pad)) {
             xFormat = "%H:%M";
@@ -133,10 +134,10 @@
             .tickSize(-height, 0)
             // .tickFormat(d3.time.format(xFormat));
 
-        // var yAxis = d3.svg.axis().scale(y).orient("left")//~important
-        //     .ticks(5)
-        //     .tickSize(-width + margin.right, margin.left)
-        //     .tickFormat(d3.time.format(yFormat));//~important
+        var yAxis = d3.svg.axis().scale(y).orient("left")//~important
+            .ticks(5)
+            .tickSize(-width + margin.right, margin.left)
+            .tickFormat(d3.time.format(yFormat));//~important
 
         var svg = d3.select("." + classd).append("svg")
             .attr("width", width + margin.left + margin.right)
@@ -151,10 +152,10 @@
             .attr("transform", "translate(" + margin.left + "," + (margin.top / 2) + ")")
             .call(xAxis);
 
-        // context.append("g")
-        //     .attr("class", "y axis")
-        //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-        //     .call(yAxis);
+        context.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + margin.left + "," + margin.top * 3 + ")")
+            .call(yAxis);
 
         var circles = context.append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
@@ -168,57 +169,59 @@
             .attr("class", "circ")
             .attr("x", function(d) {
                 // .attr("cx", function(d) {
-                return (lessThanDay(padding.pad)) ? x(d.value) : x(getDate(d.value));
+                return (lessThanDay(padding.pad)) ? x(d.value) : x(getDate(d.value)); //~~ replace .value with start and end time
             })
             .attr("y", function(d, i) {
                 // .attr("cy", function(d, i) {
-                return (lessThanDay(padding.pad)) ? y(getDate(d.value)) : y(getTime(d.value));
+                return (lessThanDay(padding.pad)) ? y(getDate(d.value)) : y(getTime(d.value)); //~~ replace .value with date from start time 
             })
             // .attr("r", 9)
-            .attr("width", 50)//~rect imp ~data //~30
+            .attr("width", 70)//~rect imp ~data //~30
             .attr("height", 40)//~rect imp ~fixed
-            .attr("rx", 3)//~imp
+            .attr("rx", 15)//~imp // set to val of width/2 if width === height
+            .attr("ry", 15)//~imp 
             // .attr("ry", 9)
             .on("click", function(d) {
+                //~~ display tool tip
                 console.log(new Date(d.value));
             })
         // ----------------------------------------- Brush ---------------------------------------------
 
-        if (enableBrush) {
-            var brush = d3.svg.brush()
-                .x(x)
-                .on("brush", _.throttle(brushed, 200));
+        // if (enableBrush) {
+        //     var brush = d3.svg.brush()
+        //         .x(x)
+        //         .on("brush", _.throttle(brushed, 200));
 
-            circles.append("g")
-                .attr("class", "brush")
-                .call(brush)
-                .selectAll("rect")
-                .attr("y", -6)
-                .attr("height", height - margin.bottom);
+        //     circles.append("g")
+        //         .attr("class", "brush")
+        //         .call(brush)
+        //         .selectAll("rect")
+        //         .attr("y", -6)
+        //         .attr("height", height - margin.bottom);
 
-            var brushEl = '<div class="brush-control"><div class="brush-info"><i>Click and drag on the timeseries to create a brush.</i></div><button class="clear-brush">Clear brush</button></div>';
-            window.document.getElementsByClassName(spaced)[0].insertAdjacentHTML('beforeend', brushEl);
+        //     var brushEl = '<div class="brush-control"><div class="brush-info"><i>Click and drag on the timeseries to create a brush.</i></div><button class="clear-brush">Clear brush</button></div>';
+        //     window.document.getElementsByClassName(spaced)[0].insertAdjacentHTML('beforeend', brushEl);
 
-            function brushed() {
-                if (!brush.empty()) {
-                    d3.select('.clear-brush').style("display", "inline-block");
-                    d3.select('.brush-info')[0][0].innerText = brush.extent();
-                }
-            }
+        //     function brushed() {
+        //         if (!brush.empty()) {
+        //             d3.select('.clear-brush').style("display", "inline-block");
+        //             d3.select('.brush-info')[0][0].innerText = brush.extent();
+        //         }
+        //     }
 
-            d3.select('.clear-brush').on("click", function(d) {
-                if (!brush.empty()) {
-                    d3.selectAll("g.brush").call(brush.clear());
-                    d3.select('.brush-info')[0][0].innerText = "";
-                    d3.select('.clear-brush').style("display", "none");
-                }
-            })
+        //     d3.select('.clear-brush').on("click", function(d) {
+        //         if (!brush.empty()) {
+        //             d3.selectAll("g.brush").call(brush.clear());
+        //             d3.select('.brush-info')[0][0].innerText = "";
+        //             d3.select('.clear-brush').style("display", "none");
+        //         }
+        //     })
 
-            timeseries.getBrushExtent = function() {
-                if (brush)
-                    return brush.extent();
-            }
-        }
+        //     timeseries.getBrushExtent = function() {
+        //         if (brush)
+        //             return brush.extent();
+        //     }
+        // }
     }
 
     /* Use this function, in conjunction to setting a time element to 'selected', to highlight the 
