5ce536e6cb29b90bd499376804eabb857e37bd12
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-04-19 07:53:41 -0400

    Update nav

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,21 +1,30 @@
 <template>
-  <div id="app">
-    <h1 class="hidden">Timbal App</h1>
-    <header class="timbalHeader">
-      <nav>
-        <h1 class="hidden">Header</h1>
-        <router-link to="/" class="imageLink"><img src="logo_gray.svg" alt="timbal logo" class="logo"></router-link>
-        <div class="profilePic"></div>
-      </nav>
-    </header>
-    <div class="testingNav">
-      <router-link to="/days">Days</router-link> | 
-      <router-link to="/systems">Systems</router-link> | 
-      <router-link to="/categories">Categories</router-link> | 
-      <router-link to="/variables">Variables</router-link> |
-      <router-link to="/signUp">Sign Up</router-link>
+  <div id="app" class="menuAndAppCon">
+    <div class="appCon">
+      <h1 class="hidden">Timbal App</h1>
+      <header class="timbalHeader">
+        <nav>
+          <h1 class="hidden">Header</h1>
+          <router-link to="/" class="imageLink"><img src="logo_gray.svg" alt="timbal logo" class="logo"></router-link>
+          <div class="profilePic"></div>
+          <div class="menuCon">
+            <h2 class="hidden">Nav menu</h2>
+            <!-- <router-link to="/queryBuilder">Query Builder</router-link> |  -->
+            <router-link to="/">Home</router-link>
+            <!-- <router-link to="/categoryLibrary">Category Library</router-link> -->
+          </div>
+        </nav>
+      </header>
+      <!-- <div class="testingNav">
+        <h2 class="hidden">test nav bar</h2>
+        <router-link to="/days">Days</router-link> | 
+        <router-link to="/systems">Systems</router-link> | 
+        <router-link to="/categories">Categories</router-link> | 
+        <router-link to="/variables">Variables</router-link> |
+        <router-link to="/signUp">Sign Up</router-link>
+      </div> -->
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
